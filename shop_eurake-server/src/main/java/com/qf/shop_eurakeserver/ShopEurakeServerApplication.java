f733be46f5dec1530d1915810d13c449ad4f27df
light <1135139884@qq.com>
2020-03-10 11:25:55 +0800

    项目架构

---
+++ shop_eurake-server/src/main/java/com/qf/shop_eurakeserver/ShopEurakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.qf.shop_eurakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEurakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEurakeServerApplication.class, args);
+    }
+
+}
---
+++ shop_eurake-server/src/test/java/com/qf/shop_eurakeserver/ShopEurakeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.shop_eurakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEurakeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
